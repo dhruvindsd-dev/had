d36f9c91b986677dd23d6007d323c71ca9fcba0a
dhruvindsd-dev <dhruvindsd@gmail.com>
2021-02-28 19:14:02 +0530

    - bug fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Switch } from "react-router";
 import "./assets/css/bulma/bulma.css";
 import "./App.css";
-import Cart from "./pages/Cart/Cart";
 import Shop from "./pages/Shop/Shop";
 import Product from "./pages/Product/Product";
 import Axios from "axios";
@@ -16,7 +15,6 @@ function App() {
   return (
     <Switch>
       <Route path="/" exact component={Shop} />
-      <Route path="/cart" exact component={Cart} />
       <Route path="/product/:id" exact component={Product} />
     </Switch>
   );
